f99a644c51dcc170c4501916f4f30dce5e10b88c
windfolgen <1801110075@pku.edu.cn>
2025-09-04 15:40:11 +0800

    manual added

---
+++ debug.m
@@ -0,0 +1,29 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{{{G[{p1, p2, p3}, {p1, p2, p3}], \[Epsilon]}, 
+  {-G[{k1, k3, p1, p2}, {k1, k3, p1, p2}], -1 + (3 + 2*\[Epsilon])/2}, 
+  {G[{k1, k3, p1, p2, p3}, {k1, k3, p1, p2, p3}], 
+   1/2 + (-3 - 2*\[Epsilon])/2}, 
+  {G[{k1, k2, k3, p1, p2}, {k1, k2, k3, p1, p2}], 
+   1/2 + (-3 - 2*\[Epsilon])/2}}, Subscript[x, 8], 
+ {k1 \[CenterDot] k1 -> Subscript[x, 1], k1 \[CenterDot] p1 -> 
+   (-p12 - Subscript[x, 1] + Subscript[x, 2])/2, 
+  k1 \[CenterDot] p2 -> (p12 - s12 - Subscript[x, 2] + Subscript[x, 3])/2, 
+  k1 \[CenterDot] p3 -> (s12 - s45 - Subscript[x, 3] + Subscript[x, 4])/2, 
+  k1 \[CenterDot] k2 -> (-Subscript[x, 1] + Subscript[x, 6] - 
+     Subscript[x, 7])/2, k2 \[CenterDot] k2 -> Subscript[x, 7], 
+  k1 \[CenterDot] p4 -> (Subscript[x, 1] - Subscript[x, 4] + 
+     Subscript[x, 10])/2, k2 \[CenterDot] p1 -> 
+   (-p12 - Subscript[x, 7] + Subscript[x, 9])/2, 
+  k2 \[CenterDot] p2 -> (p12 - s12 - Subscript[x, 9] + Subscript[x, 11])/2, 
+  k2 \[CenterDot] p3 -> (s12 + s45 + Subscript[x, 5] - Subscript[x, 6] + 
+     2*Subscript[x, 7] - Subscript[x, 8] - Subscript[x, 10] - 
+     Subscript[x, 11])/2, k2 \[CenterDot] p4 -> 
+   (-Subscript[x, 7] + Subscript[x, 8])/2, p1 \[CenterDot] p1 -> p12, 
+  p2 \[CenterDot] p2 -> 0, p3 \[CenterDot] p3 -> 0, p4 \[CenterDot] p4 -> 0, 
+  p5 \[CenterDot] p5 -> 0, p1 \[CenterDot] p2 -> (-p12 + s12)/2, 
+  p1 \[CenterDot] p3 -> (-s12 - s23 + s45)/2, p1 \[CenterDot] p4 -> 
+   (-s15 + s23 - s45)/2, p1 \[CenterDot] p5 -> (-p12 + s15)/2, 
+  p2 \[CenterDot] p3 -> s23/2, p2 \[CenterDot] p4 -> (s15 - s23 - s34)/2, 
+  p2 \[CenterDot] p5 -> (p12 - s12 - s15 + s34)/2, 
+  p3 \[CenterDot] p4 -> s34/2, p3 \[CenterDot] p5 -> (s12 - s34 - s45)/2, 
+  p4 \[CenterDot] p5 -> s45/2}}
