6c70cec076c7f6a48a2d0b7bb159f52dfe52be58
姜旭航 <1801110075@pku.edu.cn>
2026-01-22 01:09:29 +0800

    minor updates

--- init.m
+++ init.m
@@ -3,5 +3,5 @@
 $SingularFilePath::usage="Path for temporary files of Singular, which is specified by user.";
 $SingularPath::usage="Path for Singular excutable file, e.g. /usr/local/bin/Singular";
 
-$SingularFilePath="/Users/windfolgen/Documents/singular";
+$SingularFilePath="/Users/windfolgen/Documents/singular/";(*note that there must be a '/' at the end*)
 $SingularPath="/opt/homebrew/bin/Singular";
